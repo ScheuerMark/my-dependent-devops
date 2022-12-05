d4684bfc3fe53aac287a6f592a366321a6b72ef3
ScheuerMark <97870388+ScheuerMark@users.noreply.github.com>
2022-12-05 07:43:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!\n');
 });
 
-// Export server for testing.
-const server = app.listen(PORT);
-console.log(`Example app listening at http://localhost:${PORT}`);
-module.exports = server;
+const server = app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
+module.exports = {app, server};
